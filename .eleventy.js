a59e4b109a149d289d05548015239e04ad224711
ckjr3000 <charlotte.kj.roe@gmail.com>
2024-04-29 11:49:32 +0100

    Adds passthrough for config.yml

--- .eleventy.js
+++ .eleventy.js
@@ -4,6 +4,7 @@ module.exports = function(eleventyConfig){
 
     eleventyConfig.addPassthroughCopy('./src/style.css');
     eleventyConfig.addPassthroughCopy('./src/assets');
+    eleventyConfig.addPassthroughCopy('./src/admin/config.yml');
 
     // Format date 
     eleventyConfig.addFilter("postDate", (dateObj) => {
